203ca0e18535d9afa4a3077e5d8680a7d21475cb
Elvira <elvira.n.ivanova@gmail.com>
2023-10-13 11:02:57 +0300

    Added horizontal scroll functionality

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import Main from '../components/Main'
+import React from "react";
+import Main from "../components/Main";
+import Row from "../components/Row";
+import requests from "../Requests";
 
 function Home() {
   return (
     <>
-        <Main />
+      <Main />
+      <Row rowId="1" title="Up Coming" fetchURL={requests.requestUpcoming} />
+      <Row rowId="2" title="Popular" fetchURL={requests.requestPopular} />
+      <Row rowId="3" title="Trending" fetchURL={requests.requestTrending} />
+      <Row rowId="4" title="Top Rated" fetchURL={requests.requestTopRated} />
+      <Row rowId="5" title="Horror" fetchURL={requests.requestHorror} />
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
